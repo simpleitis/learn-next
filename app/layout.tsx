ea494f9433fc1d5d1be881b600afae8643a9f373
Amar Krishna <amarkrishna25@gmail.com>
2024-01-20 13:27:29 +0530

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts'
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
